19b3499d179b4c7acd32f160a2ba283cfd874551
StarlinkMobiles <timothykemoi8888@gmail.com>
2026-07-12 01:43:52 +0300

    Update Plane.jsx

--- client/src/components/Plane/Plane.jsx
+++ client/src/components/Plane/Plane.jsx
@@ -1,26 +1,79 @@
 import "./Plane.css";
 import { useGame } from "../../context/GameContext";
 import { getFlightData } from "../../utils/gameMath";
+import { useEffect, useState } from "react";
 
 function Plane() {
 
     const { game } = useGame();
 
-    const { x, y, progress } = getFlightData(game.multiplier || 1);
+    const [position, setPosition] = useState({
+    x:45,
+    y:350,
+    progress:0
+});
+
+    const [crashed, setCrashed] = useState(false);
+
+
+    useEffect(() => {
+
+        if (game.status === "flying") {
+
+            const data = getFlightData(game.multiplier || 1);
+
+            setPosition(data);
+            setCrashed(false);
+
+        }
+
+
+        if (game.status === "crashed") {
+
+            setCrashed(true);
+
+        }
+
+
+        if (game.status === "waiting") {
+
+            setPosition({
+    x: 45,
+    y: 350,
+    progress:0
+});
+
+            setCrashed(false);
+
+        }
+
+
+    }, [
+        game.status,
+        game.multiplier
+    ]);
+
+
 
     return (
 
         <div
-            className="plane"
-            style={{
-                left: `${(x / 900) * 100}%`,
-                top: `${(y / 400) * 100}%`,
-                transform: `
-               translate(-35%, -35%)
-               rotate(${-progress * 28}deg)
 
+            className={`plane ${crashed ? "plane-crash" : ""}`}
+
+            style={{
+
+                left: `${(position.x / 900) * 100}%`,
+
+                top: `${(position.y / 400) * 100}%`,
+
+                transform: `
+                    translate(-35%, -35%)
+                    rotate(${-position.progress * 28}deg)
                 `
+
             }}
+
         />
 
     );
